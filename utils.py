65a880c3055be1878e7ce8b1bfe420d0234adc0b
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-08-26 21:06:23 -0300

    Classe Utils
    Criei a classe para funções uteis

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime,timezone,timedelta
+from utilsk import passwordEncode
 from validate_email import validate_email
 import uuid
 import hashlib
@@ -8,6 +9,7 @@ import os
 load_dotenv()
 
 class Validate():
+
     def __init__(self):
         self.param = []
         self.form = {}
@@ -51,14 +53,41 @@ class Validate():
         
         return self._verifyIsEmpty()
 
-validacao = Validate()
-validacao.form={
-    "email":"ruty.pena@gmail.com",
-    # "senha":"senha"
-}
-validacao.param = ['email','senha']
 
-print(validacao.validadeForm())
+class utils():
+    # codifica senha no padrão MD5
+    def passwordEncode(self, password):
+        new_password = hashlib.md5(password.encode())
+
+        return new_password.hexdigest()
+
+    # Gera id unico randomicamente  
+    def idGenerator(self):
+        newId = str(uuid.uuid1())
+        return newId
+    
+    # verifica se o email informado é valido
+    def emailVerify(self, email):
+        validate = validate_email(email_address = email, check_regex = True, check_mx = False)
+        return validate
+
+    # Captura a data do sistema considerando fuso horario
+    def dateCapture(self):
+        date = (datetime.now().astimezone(timezone(timedelta(hours=-3)))).strftime('%Y/%m/%d')
+        return date
+
+
+# validacao = Validate()
+# validacao.form={
+#     "email":"ruty.pena@gmail.com",
+#     # "senha":"senha"
+# }
+# validacao.param = ['email','senha']
+
+# print(validacao.validadeForm())
+
+
+
 
 
 
