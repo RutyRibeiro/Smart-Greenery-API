a590954e46a426669f81fcd7882068b60e9fe284
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-09-12 16:30:46 -0300

    Create utils.py

---
+++ app/utilities/utils.py
@@ -0,0 +1,49 @@
+from datetime import datetime,timezone,timedelta
+from validate_email import validate_email
+import uuid
+import hashlib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+# Classe para funções uteis 
+class utils():
+    # codifica senha no padrão MD5
+    def passwordEncode(self, password):
+        new_password = hashlib.md5(password.encode())
+
+        return new_password.hexdigest()
+
+    # Gera id unico randomicamente  
+    def idGenerator(self):
+        newId = str(uuid.uuid1())
+        return newId
+    
+    # verifica se o email informado é valido
+    def emailVerify(self, email):
+        validate = validate_email(email_address = email, check_regex = True, check_mx = False)
+        return validate
+
+    # Captura a data do sistema considerando fuso horario
+    def dateCapture(self):
+        date = (datetime.now().astimezone(timezone(timedelta(hours=-3)))).strftime('%Y/%m/%d')
+        return date
+
+# form={
+#     "email":"ruty.pena@gmail.com",
+#     # "senha":"senha"
+# }
+# param = ['email','senha']
+# validacao= Validate()
+
+# print(validacao.validateForm(form=form, param=param))
+
+
+
+
+
+
+
+
