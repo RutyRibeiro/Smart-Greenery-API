cb0831e45c4a9bdd216345f1f71fa6351360bf81
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-09-12 16:30:37 -0300

    Create validate.py

---
+++ app/utilities/validate.py
@@ -0,0 +1,46 @@
+if __name__ == 'app.utilities.validate':
+    from ..Handlers.ResponseHandler import ResponseHandler
+else:
+    import sys
+    sys.path.append('..')
+    from Handlers.ResponseHandler import ResponseHandler
+  
+class Validate():
+
+    def __init__(self):
+        self.param = []
+        self.form = {}
+        self.responseHandler = ResponseHandler()
+
+    def _verifyExistance(self):
+        try:
+            for field in self.param:
+                if field not in self.form:
+                    raise Exception(f"O campo {field} é inexistente")
+            
+            return self.responseHandler.success(title='Sucesso',content='Campos validados com sucesso')
+
+        except Exception as error:
+            return self.responseHandler.error(title='Erro',content=str(error))
+
+    def _verifyIsEmpty(self):
+        try:
+            for field in self.param:
+                if self.form[field] is None or self.form[field] =='':
+                    raise Exception(f"O campo {field} está vazio")
+                
+            return self.responseHandler.success(title='Sucesso',content='Campos validados com sucesso')
+
+        except Exception as error:
+            return self.responseHandler.error(title='Erro',content=str(error))
+
+    def validateForm(self,form,param):
+        self.param= param
+        self.form = form
+
+        verifyExistance = self._verifyExistance()
+
+        if verifyExistance['status'] == 'erro':
+            return verifyExistance
+        
+        return self._verifyIsEmpty()
