f38913d6f2359520e9057517e03d02ff039e5764
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-09-22 00:36:35 -0300

    Update sendEmail.py
    Atualizei formato de email padrão

--- app/utilities/sendEmail.py
+++ app/utilities/sendEmail.py
@@ -13,14 +13,15 @@ responseHandler = ResponseHandler()
 
 load_dotenv()
 
+
 class SendEmail():
-    
+
     def __init__(self):
-            self.host = os.getenv("EMAILHOST")
-            self.port = os.getenv("EMAILPORT")
-            self.user = os.getenv("EMAILUSER")
-            self.password = os.getenv("EMAILPASSWORD")
-            self.server = smtplib.SMTP(self.host, self.port)
+        self.host = os.getenv("EMAILHOST")
+        self.port = os.getenv("EMAILPORT")
+        self.user = os.getenv("EMAILUSER")
+        self.password = os.getenv("EMAILPASSWORD")
+        self.server = smtplib.SMTP(self.host, self.port)
 
     def _loginEmail(self):
         try:
@@ -32,30 +33,48 @@ class SendEmail():
         except Exception as error:
             return responseHandler.error(content=error)
 
-    def sendEmail(self,token,email):
+    def sendEmail(self, token, email):
         try:
             login = self._loginEmail()
             if login['status'] == 'erro':
                 return login
             message = f'''
-                            <div style="textAlign:center">
-                            <h2>Olá recupere sua conta Smart-Greenery</h2>
-                            <h4>Seu código de acesso é {token}</h4>
+                            <div style="width:100%;background:#ededed;padding:25px 0">
+                            <table style=" border:2px solid #2f2f2f4a;
+                            border-top:2px solid #0FFF00;
+                            border-bottom:none;
+                            margin:0 auto;
+                            padding:25px 5px;
+                            text-align:center;
+                            max-width:620px;width:80%;background:#fff;    box-shadow: 4px 9px 15px -10px;">
                             
+                            <tr>
+                                <td>Olá recupere sua conta na <span style="color:#0EAD2B; font-weight:bold">Smart-Greenery</span></td>
+                            </tr>
+                            <tr>
+                                <td>Seu código de acesso é:</td>
+                            </tr>
+                            <tr style="margin-top:25px">
+                                <td style="font-size:1.9rem;font-weight:bold">{token}</td>
+                            </tr>
+                            
+                            </table>
                             </div>
                     '''
             email_msg = MIMEMultipart()
             email_msg['From'] = self.user
             email_msg['To'] = email
-            email_msg['Subject'] = 'SMART-GREENERY - Recuperação de Conta'
+            email_msg['Subject'] = 'Recupere sua conta em Smart-Greenery'
             email_msg.attach(MIMEText(message, 'html'))
 
-            self.server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+            self.server.sendmail(
+                email_msg['From'], email_msg['To'], email_msg.as_string())
             self.server.quit()
 
-            return responseHandler.success('Email enviado com Sucesso!')
+            return responseHandler.success('Email enviado com Sucesso! Verifique sua caixa de entrada ou spam')
         except Exception as error:
             return responseHandler.error(content=error)
 
-em = SendEmail()
-print(em.sendEmail(token='213124234',email='rribeiropena@gmail.com'))
+
+# em = SendEmail()
+# print(em.sendEmail(token='213124234', email='rribeiropena@gmail.com'))
