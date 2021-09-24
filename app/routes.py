7d5c3601463bf57d41727205e2260f145af461f9
Ruty Ribeiro <64114456+RutyRibeiro@users.noreply.github.com>
2021-09-23 22:38:52 -0300

    Update routes.py
    Ajustes nos responses + Rota de confirm retrieve

--- app/routes.py
+++ app/routes.py
@@ -45,50 +45,56 @@ def retrieveUser():
     email = request.headers.get('email') or False 
     
     if(email == False):
-        return responseHandler.error('Erro', 'Não foram enviadas as informações de email necessárias nos headers'), 400
+        return responseHandler.error(content='Não foram enviadas as informações de email necessárias nos headers'), 400
     
     response = user.retrieve(user=email)
     return response
 
-@app.route('/plant/register', methods=['POST'])
+@app.route('/user/confirmretrieve/', methods=['POST'])
+@cross_origin(origin='*',headers=['Content-Type'])
+def confirmRetrieve():
+    response = user.confirmRetrieve(user=request.get_json())
+    return response
+
+@app.route('/plant/register/', methods=['POST'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def registerPlant():
     response = plant.createPlant(plant=request.get_json())
     return response
 
-@app.route('/plant/delete', methods=['DELETE'])
+@app.route('/plant/delete/', methods=['DELETE'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def deletePlant():
     plantId = request.headers.get('id-planta') or False 
     if(plantId == False):
-        return responseHandler.error('Erro', 'Não foram enviadas as informações de planta necessárias nos headers'), 400
+        return responseHandler.error(content = 'Não foram enviadas as informações de planta necessárias nos headers'), 400
     
     response = plant.deletePlant(idPlant=plantId) 
     
     return  response
 
-@app.route('/plant/modify', methods=['PUT'])
+@app.route('/plant/modify/', methods=['PUT'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def modifyPlant():
     response = plant.modifyPlant(plant = request.get_json())
     return response
 
-@app.route('/greenery/get', methods=['GET'])
+@app.route('/greenery/get/', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def getGreenery():
     userId = request.headers.get('user-id') or False 
     if(userId == False):
-        return responseHandler.error('Erro', 'Não foram enviadas as informações de estufa necessárias nos headers'), 400
+        return responseHandler.error(content = 'Não foram enviadas as informações de estufa necessárias nos headers'), 400
    
     return  greenery.getGreenerys(userId=userId)
 
-@app.route('/greenery/modify', methods=['PUT'])
+@app.route('/greenery/modify/', methods=['PUT'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def modifyGreenery():
     response = greenery.modifyGreenery(request.get_json())
     return response
 
-@app.route('/element/modify', methods=['PUT'])
+@app.route('/element/modify/', methods=['PUT'])
 @cross_origin(origin='*',headers=['Content-Type'])
 def modifyElement():
     response = element.modifyElement(request.get_json())
